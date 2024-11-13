a999a5623de700b0c822fc6c8d1b01aa6cfd56f1
MiniKao <52775937+kao273183@users.noreply.github.com>
2024-11-13 16:10:37 +0800

    Update sendTG.py

--- function_tw/sendTG.py
+++ function_tw/sendTG.py
@@ -3,7 +3,7 @@ from datetime import datetime
 def sendMessage():
     currentDateAndTime = datetime.now()
     currentTime = currentDateAndTime.strftime("%Y-%m-%d %H:%M:%S")
-    token='6941252285:AAF6lTxqeF2LjUb9zaic1Scx7xmbB0-gkSU'
+    token=''
     chat_id='-4104404244'
     text='Jenkins通知:\n測試時間:' + currentTime + '\n模擬交易測試完畢'
     bot = telegram.Bot(token=token)
